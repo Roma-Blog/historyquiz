0c64a7f4a88edf62346c14aee76ae84aee621cbf
roma <roma@mail.ru>
2025-06-20 17:22:52 +0700

    app-models-tempaltes-static

--- quiz/historyquiz/models.py
+++ quiz/historyquiz/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Категории вопроса
+class CategoryQuestions(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.CharField(max_length=320, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+#Вопросы
+class Question(models.Model):
+    text = models.TextField()
+    category = models.ForeignKey(CategoryQuestions, on_delete=models.CASCADE, related_name='questions')
+    correct_answer = models.CharField(max_length=128)
+    incorrect_answers = models.JSONField()
+    image = models.ImageField(upload_to='questions_images/', blank=True, null=True)
+    favorites = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+#Для сбора статистики по ответам на вопросы
+class StatisticsOnIssues (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    count_true_answer = models.IntegerField(default=0)
+    count_false_answer = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Statistics for {self.user.username} on question {self.question.id}"
+
